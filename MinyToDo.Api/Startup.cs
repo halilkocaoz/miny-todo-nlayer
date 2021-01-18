8da5d666dfd6d1e16cceef67ce3559a6dde93592
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-01-18 15:04:24 +0300

    Added DbContext as Service.

--- MinyToDo.Api/Startup.cs
+++ MinyToDo.Api/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MinyToDo.Data;
 
 namespace MinyToDo.Api
 {
@@ -31,6 +33,11 @@ namespace MinyToDo.Api
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MinyToDo.Api", Version = "v1" });
+            });
+
+            services.AddDbContext<MinyToDoContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("dev"));
             });
         }
 
