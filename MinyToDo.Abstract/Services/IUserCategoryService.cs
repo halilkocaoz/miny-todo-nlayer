c42bc1d1af758a4fe57ee882aad77222327acbf3
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-01-22 03:55:57 +0300

    Implemented DTO returns for UserCategoryService

--- MinyToDo.Abstract/Services/IUserCategoryService.cs
+++ MinyToDo.Abstract/Services/IUserCategoryService.cs
@@ -2,19 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MinyToDo.Entity.DTO.Request;
+using MinyToDo.Entity.DTO.Response;
 using MinyToDo.Entity.Models;
 
 namespace MinyToDo.Abstract.Services
 {
     public interface IUserCategoryService
     {
-        // todo: change return types to data transfer object
-        Task<UserCategory> InsertAsync(Guid appUserId, UserCategoryRequest newCategory);
-        Task<UserCategory> UpdateAsync(UserCategory userCategory, UserCategoryRequest newValues);
+        Task<UserCategoryResponse> InsertAsync(Guid appUserId, UserCategoryRequest newCategory);
+        Task<UserCategoryResponse> UpdateAsync(UserCategory userCategory, UserCategoryRequest newValues);
         Task<bool> DeleteAsync(UserCategory userCategory);
 
-        Task<IEnumerable<UserCategory>> GetAllByUserId(Guid appUserId);
-        Task<IEnumerable<UserCategory>> GetAllWithTasksByUserId(Guid appUserId);
+        Task<IEnumerable<UserCategoryResponse>> GetAllByUserId(Guid appUserId);
+        Task<IEnumerable<UserCategoryResponse>> GetAllWithTasksByUserId(Guid appUserId);
 
         Task<UserCategory> GetById(Guid categoryId);
     }
--- MinyToDo.Service/Concrete/UserCategoryService.cs
+++ MinyToDo.Service/Concrete/UserCategoryService.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.Extensions.Logging;
 using MinyToDo.Abstract.Repositories;
 using MinyToDo.Abstract.Services;
 using MinyToDo.Entity.DTO.Request;
+using MinyToDo.Entity.DTO.Response;
 using MinyToDo.Entity.Models;
 
 namespace MinyToDo.Service.Concrete
@@ -12,23 +14,24 @@ namespace MinyToDo.Service.Concrete
     public class UserCategoryService : IUserCategoryService
     {
         private IUserCategoryRepository _userCategoryRepository;
-        private ILogger _logger;
+        private IMapper _mapper;
 
-        public UserCategoryService(IUserCategoryRepository userCategoryRepository)
+        public UserCategoryService(IUserCategoryRepository userCategoryRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _userCategoryRepository = userCategoryRepository;
         }
 
-        public async Task<UserCategory> InsertAsync(Guid appUserId, UserCategoryRequest newCategory)
+        public async Task<UserCategoryResponse> InsertAsync(Guid appUserId, UserCategoryRequest newCategory)
         {
             var newEntity = new UserCategory(appUserId, newCategory);
-            return await _userCategoryRepository.InsertAsync(newEntity);
+            return _mapper.Map<UserCategoryResponse>(await _userCategoryRepository.InsertAsync(newEntity)); 
         }
 
-        public async Task<UserCategory> UpdateAsync(UserCategory userCategory, UserCategoryRequest newValues)
+        public async Task<UserCategoryResponse> UpdateAsync(UserCategory userCategory, UserCategoryRequest newValues)
         {
             userCategory.Name = newValues.Name;
-            return await _userCategoryRepository.UpdateAsync(userCategory);
+            return _mapper.Map<UserCategoryResponse>(await _userCategoryRepository.UpdateAsync(userCategory));
         }
 
         public async Task<bool> DeleteAsync(UserCategory userCategory)
@@ -36,14 +39,14 @@ namespace MinyToDo.Service.Concrete
             return await _userCategoryRepository.DeleteAsync(userCategory) > 0 ? true : false;
         }
 
-        public async Task<IEnumerable<UserCategory>> GetAllByUserId(Guid appUserId)
+        public async Task<IEnumerable<UserCategoryResponse>> GetAllByUserId(Guid appUserId)
         {
-            return await _userCategoryRepository.GetAll(x => x.ApplicationUserId == appUserId);
+            return _mapper.Map<IEnumerable<UserCategoryResponse>>(await _userCategoryRepository.GetAll(x => x.ApplicationUserId == appUserId));
         }
 
-        public async Task<IEnumerable<UserCategory>> GetAllWithTasksByUserId(Guid appUserId)
+        public async Task<IEnumerable<UserCategoryResponse>> GetAllWithTasksByUserId(Guid appUserId)
         {
-            return await _userCategoryRepository.GetAllWithTasksAsync(x => x.ApplicationUserId == appUserId);
+            return _mapper.Map<IEnumerable<UserCategoryResponse>>(await _userCategoryRepository.GetAllWithTasksAsync(x => x.ApplicationUserId == appUserId));
         }
 
         public async Task<UserCategory> GetById(Guid categoryId)
