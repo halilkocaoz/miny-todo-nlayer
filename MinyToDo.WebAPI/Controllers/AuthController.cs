ccb8f773b452dccefc4cf37eada811445c9ba2d2
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-07-17 00:18:10 +0300

    Update: routes, swagger.png

--- MinyToDo.WebAPI/Controllers/AuthController.cs
+++ MinyToDo.WebAPI/Controllers/AuthController.cs
@@ -9,42 +9,42 @@ using MinyToDo.Models.Entity;
 
 namespace MinyToDo.WebAPI.Controllers
 {
-    [Route("Api/[controller]")]
+    [Route("auth")]
     public class AuthController : ControllerBase
     {
-        private readonly UserManager<AppUser> _userManager;
-        private readonly IJwtTokenService _jwtTokenService;
-        private readonly IUserCategoryService _userCategoryService;
-        private readonly IMapper _mapper;
+        private readonly UserManager<AppUser> userManager;
+        private readonly IJwtTokenService jwtTokenService;
+        private readonly IUserCategoryService userCategoryService;
+        private readonly IMapper mapper;
         public AuthController(IMapper mapper, IJwtTokenService jwtTokenService, UserManager<AppUser> userManager, IUserCategoryService userCategoryService)
         {
-            _mapper = mapper;
-            _jwtTokenService = jwtTokenService;
-            _userManager = userManager;
-            _userCategoryService = userCategoryService;
+            this.mapper = mapper;
+            this.jwtTokenService = jwtTokenService;
+            this.userManager = userManager;
+            this.userCategoryService = userCategoryService;
         }
 
-        [HttpPost("Signup")]
+        [HttpPost("signup")]
         public async Task<IActionResult> SignUp([FromBody] SignUpRequest value)
         {
             if (User.Identity.IsAuthenticated) return BadRequest();
 
-            var newAppUser = _mapper.Map<AppUser>(value);
+            var newAppUser = mapper.Map<AppUser>(value);
 
-            var result = await _userManager.CreateAsync(newAppUser, value.Password);
+            var result = await userManager.CreateAsync(newAppUser, value.Password);
             if (result.Succeeded)
             {
                 #pragma warning disable 4014 // creating default UserCategory for new signing up user.
-                _userCategoryService.InsertAsync(newAppUser.Id, new UserCategoryRequest { Name = "General" });
+                userCategoryService.InsertAsync(newAppUser.Id, new UserCategoryRequest { Name = "General" });
                 #pragma warning disable 4014
 
-                var token = _jwtTokenService.CreateToken(newAppUser);
+                var token = jwtTokenService.CreateToken(newAppUser);
                 return Ok(new { token });
             }
             return BadRequest(result.Errors);
         }
 
-        [HttpPost("Signin")]
+        [HttpPost("signin")]
         public async Task<IActionResult> SignIn([FromBody] SignInRequest value)
         {
             if (User.Identity.IsAuthenticated) return BadRequest();
@@ -52,13 +52,13 @@ namespace MinyToDo.WebAPI.Controllers
             var isIdentifierEmail = value.Identifier.Contains('@');
 
             var appUser = isIdentifierEmail
-            ? await _userManager.FindByEmailAsync(value.Identifier)
-            : await _userManager.FindByNameAsync(value.Identifier);
+            ? await userManager.FindByEmailAsync(value.Identifier)
+            : await userManager.FindByNameAsync(value.Identifier);
 
-            var isPasswordRelatedToFoundUser = appUser != null && await _userManager.CheckPasswordAsync(appUser, value.Password);
+            var isPasswordRelatedToFoundUser = appUser != null && await userManager.CheckPasswordAsync(appUser, value.Password);
             if (isPasswordRelatedToFoundUser)
             {
-                var token = _jwtTokenService.CreateToken(appUser);
+                var token = jwtTokenService.CreateToken(appUser);
                 return Ok(new { token });
             }
             return Unauthorized();
